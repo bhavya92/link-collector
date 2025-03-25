a6a18444bc9ec560e3382fb91bd3c3da2b415e4e
bhavya92 <bhavyasharma535@gmail.com>
2025-03-26 00:09:02 +0530

    bug fix

--- backend/src/utils/linkType/categorizeBuUrl.ts
+++ backend/src/utils/linkType/categorizeBuUrl.ts
@@ -1,8 +1,6 @@
 import { domainCategoryMap } from "./categoryMaps.js";
 
 export const categorizeByDomaint = (url: string) => {
-    const domain = new URL(url).hostname.replace("www","");
+    const domain = new URL(url).hostname.replace("www.","");
     return domainCategoryMap[domain] || null;
 }
-
-//TODO : Implement AI categorization 
